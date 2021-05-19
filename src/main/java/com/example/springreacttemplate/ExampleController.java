e99b8189337c92d0081dfa71fe87e484f9cf1f4f
DaeYoung Nam <wsnam0507@naver.com>
2021-05-19 14:23:02 +0900

    springboot - react sample project

---
+++ src/main/java/com/example/springreacttemplate/ExampleController.java
@@ -0,0 +1,28 @@
+package com.example.springreacttemplate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ExampleController {
+
+    @GetMapping(path = "/api/hello")
+    public Map<String, String> hello() {
+        Map<String, String> json = new HashMap<>();
+        json.put("message", "Spring - React Project templates!! " + new SimpleDateFormat("yyyy/MM/dd hh:mm:ss").format(new Date()));
+        json.put("url", "https://github.com/devdynam0507/spring-react-template");
+
+        return json;
+    }
+
+    public String index() {
+        return "index.html";
+    }
+
+}
